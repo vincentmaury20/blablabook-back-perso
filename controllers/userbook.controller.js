b4eb9da3cd572b4cb3f06e73fa08b497aa81c1a7
Claude Ros <ros.claude.8@gmail.com>
2025-10-03 23:10:35 +0200

    Save progression

--- controllers/userbook.controller.js
+++ controllers/userbook.controller.js
@@ -4,6 +4,10 @@ import { Sequelize } from 'sequelize';
 export const userbookController = {
 
     async getBooks(req, res) {
+        const page = parseInt(req.query.page, 10) || 1;
+        const limit = parseInt(req.query.limit, 10) || 10;
+        const offset = (page - 1) * limit;        
+
         try {
             const { userId } = req.params;
             const user = await User.findByPk(userId);
@@ -11,25 +15,34 @@ export const userbookController = {
                 where: { user_id: user.id },
                 include: [
                     { model: Book, as: "book" },
-                ]
+                ],
+                order: [[{ model: Book, as: "book"}, 'title', 'ASC']],
+                limit,
+                offset
             });
-
+            
             if (!user) {
                 return res.status(404).json({ error: 'User not found' });                
             }
-            res.json(userbooks);
+
+            const totalBooks = await UserBook.count({
+                where: { user_id: user.id }    
+            });
+            const totalPages = Math.ceil(totalBooks / limit);
+
+            res.json({
+                page,
+                totalPages,
+                totalBooks,
+                userbooks
+            });
         } catch (error) {
             console.log(error);
             res.status(500).json({ error: 'Erreur lors de la récupération de la booklist' });
         }
 
     }
-   //       if (!user || !book) {
-   //          return res.status(404).json({ error: 'User or book not found' });
-   //       }
 
-   //       await user.addBook(book);
-   //       res.status(200).json({ message: 'Book successfully added to user' });
 
 
    //  notre méthode ici permet d'ajouter un livre à la booklist
