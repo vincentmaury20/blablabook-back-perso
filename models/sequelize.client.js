4346f2048c3850623138836c079f28e981781255
Vincent <vincentmaury20@gmail.com>
2026-03-26 01:09:16 +0100

    Fix Sequelize DATABASE_URL + SSL

--- models/sequelize.client.js
+++ models/sequelize.client.js
@@ -1,8 +1,16 @@
 import "dotenv/config";
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize(process.env.DB_URL, {
+export const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
   logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
   define: {
     createdAt: "created_at",
     updatedAt: "updated_at",
